d05c2231704ccf7a975dc2c972d4743b0439dfde
Nick Apperley <napperley@gmail.com>
2017-04-29 15:27:05 +1200

    Added reduce example.

--- src/main/kotlin/org/example/fpexercises/main.kt
+++ src/main/kotlin/org/example/fpexercises/main.kt
@@ -1,13 +1,20 @@
 package org.example.fpexercises
 
 fun main(args: Array<String>) {
-    doMap()
+    doReduce()
 }
 
-@Suppress("ConvertLambdaToReference")
+@Suppress("ConvertLambdaToReference", "unused")
 private fun doMap() {
     // Transform each element in the array from a String to Hash Code.
     val codeNames = arrayOf("Mary", "Isla", "Sam").map { it.hashCode() }
 
     codeNames.forEach { println("Code Name: $it") }
+}
+
+private fun doReduce() {
+    // Combine all elements in the List into a single result.
+    val sum = (0..4).toList().reduce { a, x -> a + x }
+
+    println("Sum: $sum")
 }
